7233f3ac7f7d19cb83e18a946c8ba6ab8c237ffa
Dmitry <enoughfornowbusiness@gmail.com>
2025-01-10 11:35:33 +0300

    feat: добавил actions в стор

--- src/features/example/store.ts
+++ src/features/example/store.ts
@@ -1,10 +1,19 @@
 export const useCounterStore = defineStore('counter', () => {
+  // state
   const count = ref(0);
   const name = ref('Eduardo');
+
+  // getters
   const doubleCount = computed(() => count.value * 2);
+
+  // actions
   function increment() {
     count.value++;
   }
+  async function asyncIncrement() {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    count.value++;
+  }
 
-  return { count, name, doubleCount, increment };
+  return { count, name, doubleCount, increment, asyncIncrement };
 });
